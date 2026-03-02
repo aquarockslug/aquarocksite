99651739c761a7cee4bff1a1652e8f5e11f5965f
aqua <aqua.rock.slug@gmail.com>
2026-03-01 22:16:04 -0600

    reorg

---
+++ js/games.js
@@ -0,0 +1,17 @@
+const games = [
+	{
+		name: "Star Checkers",
+		url: "games/star-checkers/star-checkers.png",
+		type: "board",
+		description:
+			"be the first to move all 10 of your marbles from your starting triangle to the opposite corner",
+	},
+	{
+		name: "Gas Mask",
+		url: "games/gas-game/gas-game.png",
+		type: "puzzle",
+		description: "I haven't added this game to the site yet",
+	},
+];
+
+export default games;
--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-const games = [
-	{
-		name: "Star Checkers",
-		url: "games/star-checkers/star-checkers.png",
-		type: "board",
-		description:
-			"be the first to move all 10 of your marbles from your starting triangle to the opposite corner",
-	},
-	{
-		name: "Gas Mask",
-		url: "games/gas-game/gas-game.png",
-		type: "puzzle",
-		description: "I haven't added this game to the site yet",
-	},
-];
+import games from "./games.js";
 
 const gamesWrapperEl = document.getElementById("games-wrapper");
 const checkEls = document.querySelectorAll(".check");
